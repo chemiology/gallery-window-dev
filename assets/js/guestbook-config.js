f025180c1db77730600d7a683ef271cee881c59d
chemiology <172052088+chemiology@users.noreply.github.com>
2026-05-30 23:14:07 +0900

    Add files via upload

--- assets/js/guestbook-config.js
+++ assets/js/guestbook-config.js
@@ -1,2 +1,2 @@
 // Guestbook API endpoint (Google Apps Script Web App)
-window.GUESTBOOK_URL = 'https://script.google.com/macros/s/AKfycbxosBRSAcNGTRHQXIm6EIVQKEnFhFlK4jMGpIDFnccUfwXcLdRZO2vKn3GsmQvMjo8raQ/exec';
+window.GUESTBOOK_URL = 'https://script.google.com/macros/s/AKfycbyGN2wMafi8iYsOUPNDY9CkbgkyJ3snO6ac7_GIsyBA4R9FzxLWsbjvn9EdSsXH2TFwtw/exec';
